751885fcb35b0179d25c5361dd6ddb5967598d60
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-12-16 11:30:51 -0800

    Layout is finished.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import DataTable from './dataTable/DataTable';
+import InstrumentChip from './chipSelectors/InstrumentChip';
+import DateChip from './chipSelectors/DateChip';
+import SpeciesChip from './chipSelectors/SpeciesChip';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import UploadIcon from '@mui/icons-material/Upload';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 
 function App() {
+  // Need to modify state here for the chip selectors
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained" size="small" endIcon={<UploadIcon />} sx={{padding:"10px", margin: "10px", left:"10px"}}>Import Data</Button>
+      <Stack spacing={2} direction="row" sx={{position: "relative", padding: "10px"}}>
+        <DateChip />
+        <SpeciesChip />
+        <InstrumentChip />
+        <Button variant="contained" color="success" size="small" endIcon={<CheckBoxIcon /> }>Apply filter</Button>
+        <Button variant="contained" size="small" endIcon={<FileDownloadIcon />}>Save as CSV</Button>
+      </Stack>
+      <div style={{ display: 'flex', height: '100%', position: "relative"}} >
+        <DataTable />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import DataTable from './dataTable/DataTable'
 
 ReactDOM.render(
   <React.StrictMode>
